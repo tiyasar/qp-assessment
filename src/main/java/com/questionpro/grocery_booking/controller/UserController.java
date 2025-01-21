9567a75a7b283240b4c459d51885ecd77291fee0
Tiyasa Roy <tiyasa@unifiedinfotech.net>
2025-01-21 10:19:01 +0530

    User API

--- src/main/java/com/questionpro/grocery_booking/controller/UserController.java
+++ src/main/java/com/questionpro/grocery_booking/controller/UserController.java
@@ -2,7 +2,9 @@ package com.questionpro.grocery_booking.controller;
 
 import com.questionpro.grocery_booking.dto.ApiResponse;
 import com.questionpro.grocery_booking.dto.OrderDto;
+import com.questionpro.grocery_booking.entity.GroceryItem;
 import com.questionpro.grocery_booking.entity.Order;
+import com.questionpro.grocery_booking.service.GroceryItemService;
 import com.questionpro.grocery_booking.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -12,18 +14,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user/order")
+@RequestMapping("/user")
 @Slf4j
 public class UserController {
     private final OrderService orderService;
+    private final GroceryItemService groceryItemService;
 
     @Autowired
-    public UserController(OrderService orderService) {
+    public UserController(OrderService orderService, GroceryItemService groceryItemService) {
         this.orderService = orderService;
+        this.groceryItemService = groceryItemService;
     }
 
-    @PostMapping
+    @PostMapping("order")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<Order> createOrder(@RequestBody @Valid OrderDto orderDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -32,6 +38,18 @@ public class UserController {
                 .statusCode(HttpStatus.OK.value())
                 .message("Order placed successfully")
                 .data(order)
+                .error(null)
+                .build();
+    }
+
+    @GetMapping("grocery")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<List<GroceryItem>> getGroceryItems() {
+        List<GroceryItem> groceryItems = groceryItemService.getGroceryItems();
+        return ApiResponse.<List<GroceryItem>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Grocery items fetched successfully")
+                .data(groceryItems)
                 .error(null)
                 .build();
     }
